1914f55c9758bda2242b1b0bbdbcee19e5ef897b
rajkumar1447 <rajkumarbetham7@gmail.com>
2025-07-25 15:51:56 +0530

    Floder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+mkdir -p src
+mkdir -p research
+
+# Creating files.
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/traials.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo "Directory and files created successfully."
