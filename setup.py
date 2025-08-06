006561bfba6e743aab46fc99e1fbaff2cc7ce8be
rajkumar1447 <rajkumarbetham7@gmail.com>
2025-08-06 10:00:12 +0530

    Setup file and packages added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Betham Raj kumar",
+    author_email="rajkumarbetham7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
